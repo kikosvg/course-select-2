fa2fe5cdef4c524fac6909bf03575ae7195e9652
kikosvg <xkanyangzi@gmail.com>
2025-02-28 15:30:37 -0500

    .

--- src/views/about/document/api/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <GiIframe src="https://api.continew.top/doc.html"></GiIframe>
-</template>
-
-<script lang='ts' setup>
-defineOptions({ name: 'AboutDocumentApi' })
-</script>
-
-<style scoped lang="scss"></style>
